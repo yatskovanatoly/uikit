861c55be147373f5d4374cbd9406d1ed9da705eb
Kezhik Kyzyl-ool <k.kyzylool@nebius.com>
2024-02-12 14:05:00 +0100

    feat: adding title prop for Select, additional titles in stories

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -88,6 +88,7 @@ export const Select = React.forwardRef<HTMLButtonElement, SelectProps>(function 
         hasClear = false,
         onClose,
         id,
+        title,
     } = props;
     const mobile = useMobile();
     const [{filter}, dispatch] = React.useReducer(reducer, initialState);
@@ -341,6 +342,7 @@ export const Select = React.forwardRef<HTMLButtonElement, SelectProps>(function 
                 popupId={`select-popup-${selectId}`}
                 selectId={`select-${selectId}`}
                 activeIndex={activeIndex}
+                title={title}
             />
 
             <SelectPopup
